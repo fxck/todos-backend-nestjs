d0b568a60eecfc01643375a77a7c2c3fcb6efbc2
Yoshi <kawaii.yoshiyuki@gmail.com>
2022-01-13 21:00:19 +0100

    set cors

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
-  // app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  await app.listen(5000);
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: `${
+      process.env.APP_ENV === 'production'
+        ? process.env.FE_URL
+        : 'http://localhost:3000'
+    }`,
+  });
+
+  await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
