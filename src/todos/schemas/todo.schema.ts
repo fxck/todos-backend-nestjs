c8b88f97da35283660211e41a59fddeda1140014
Aleš <foxxcz@gmail.com>
2022-01-19 22:18:10 +0100

    Update todo.schema.ts

--- src/todos/schemas/todo.schema.ts
+++ src/todos/schemas/todo.schema.ts
@@ -5,9 +5,9 @@ export const TodoSchema = new mongoose.Schema(
     text: String,
     index: Number,
     completed: Boolean,
-  },
-  toJSON: { virtuals: true },
-  toObject: { virtuals: true }  
+  }
 );
 
 TodoSchema.virtual('id').get(function() { return this._id.toHexString(); });
+TodoSchema.set('toObject', { virtuals: true });
+TodoSchema.set('toJSON', { virtuals: true });
